94e29c0ebc9881f3421a5ff0bde7c9b04efcf07e
pinzhenx <pinzhen.xu@intel.com>
2019-03-25 23:52:28 +0800

    deploy suicide worker in base.js

--- examples/util/base.js
+++ examples/util/base.js
@@ -649,3 +649,15 @@ const getSearchParamsModel = () => {
     return '';
   }
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/webml-demo/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
